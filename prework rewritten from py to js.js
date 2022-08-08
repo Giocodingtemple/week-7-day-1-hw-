0d0ca1d6dd66a2459193184d3885c2288c6036f2
Giocodingtemple <gmarchiori5149@gmail.com>
2022-08-08 18:40:22 -0500

    Easy Script so far but i know it will only get harder

--- prework rewritten from py to js.js
+++ prework rewritten from py to js.js
@@ -8,6 +8,7 @@ function hello_username(username) {
 }
 
 hello_username("username");
+console.log("==========================");
 
 // -----------Question 2
 // Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing    
@@ -21,18 +22,27 @@ function first_odds() {
 }
 
 first_odds();
+console.log("==========================");
 
 // -----------Question 3
 // Please write a Python function, max_num_in_list to return the max number of a given list.
-function max_num_in_list(a_list) {
-  /* Finds the highest number in a given list and returns that number */
-  var max_num;
-  max_num = max(a_list);
-  return max_num;
-}
-
-my_list = [1, 3, 4, 14, 85, 25, 35, 42];
-console.log(max_num_in_list(my_list));
+function max_num_in_list(list) {
+    var max;
+    max = list[0];
+  
+    for (var a, _pj_c = 0, _pj_a = list, _pj_b = _pj_a.length; _pj_c < _pj_b; _pj_c += 1) {
+      a = _pj_a[_pj_c];
+  
+      if (a > max) {
+        max = a;
+      }
+    }
+  
+    return max;
+  }
+  
+console.log(max_num_in_list([1, 3, 4, 14, 85, 25, 35, 42]));
+console.log("==========================");
 
 // -----------Question 4
 // Write a function to return if the given year is a leap year. 
@@ -49,27 +59,23 @@ function is_leap_year(a_year) {
 }
 
 console.log(is_leap_year(2012));
+console.log(is_leap_year(2016));
+console.log(is_leap_year(2020));
+console.log(is_leap_year(2022));
+console.log("==========================");
+
 
 // -----------Question 5
 // Write a function to check to see if all numbers in list are consecutive numbers.
-function is_consectutive(a_list) {
-  var check;
-  check = 0;
-  a_list = [1, 2, 3, 4, 5, 6, 7, 8];
-
-  for (var index = 0, _pj_a = a_list.length; index < _pj_a; index += 1) {
-    if (index !== max(range(a_list.length))) {
-      if (a_list[index] + 1 !== a_list[index + 1]) {
-        check += 1;
-      }
+function is_consectutive(test_list) {
+    test_list = test_list.slice(0, test_list.length - 1);
+    for (var i = 0; i < test_list.length; i++) {
+        if (test_list[i] !== test_list[i + 1]) {
+            return false;
+        }
+    
     }
-  }
-
-  if (check >= 1) {
-    return false;
-  } else {
-    return true;
-  }
 }
 
+test_list = [1, 2, 3, 4, 5, 6, 7, 8];
 console.log(is_consectutive(test_list))
